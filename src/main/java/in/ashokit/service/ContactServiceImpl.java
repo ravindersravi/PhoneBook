e1de86b8d3f078260c401bbe2b2b1345cb2fa408
Ravi <62902720+ravindersravi@users.noreply.github.com>
2022-04-19 15:11:26 +0530

    Update ContactServiceImpl.java

--- src/main/java/in/ashokit/service/ContactServiceImpl.java
+++ src/main/java/in/ashokit/service/ContactServiceImpl.java
@@ -1,5 +1,43 @@
 package in.ashokit.service;
 
-public class ContactServiceImpl implements {
+import in.ashokit.entiry.Contact;
+import in.ashokit.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class ContactServiceImpl implements ContactService{
+
+    @Autowired
+    ContactRepository contactRepository;
+
+    @Autowired
+    public ContactServiceImpl(ContactRepository contactRepository){
+        this.contactRepository = contactRepository;
+    }
+
+    @Override
+    public Contact getContact(int cid) {
+        return contactRepository.getById(cid);
+    }
+
+    @Override
+    public List<Contact> getAllContacts() {
+        return  contactRepository.findAll();
+    }
+
+    @Override
+    public Contact upsert(Contact contact) {
+        return contactRepository.save(contact);
+    }
+
+    @Override
+    public String deleteContact(Integer cid) {
+
+         contactRepository.deleteById(cid);
+
+         return "success";
+    }
 }
